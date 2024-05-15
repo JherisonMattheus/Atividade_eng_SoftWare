df61007bd9f1d154bdb86e56a49babe6bfe12c50
JherisonMattheus <jherisonmattheus.lemos@gmail.com>
2024-05-15 10:20:33 -0300

    falta a tela de jogo e a linkagem com o historico

--- script.js
+++ script.js
@@ -1,53 +1,99 @@
 const historico = document.getElementById('historico');
 const play = document.getElementById('play');
+const comecar = document.getElementById('button-nickname');
 const sair = document.getElementById('sair');
 
 const main_screen = document.getElementById('main-screen');
 const sec_historico = document.getElementById('sec-historico');
 const title = document.getElementById('title');
 const game = document.getElementById('game');
+const register = document.getElementById('nickname-register');
+const tela_jogo = document.getElementById('tela-jogo');
 
 
 historico.addEventListener('click', ()=>{
     if(sec_historico.classList.contains('hide')){
+        main_screen.classList.add('main-screen-alt');
+
+        historico.style.pointerEvents = 'none';
+        sec_historico.classList.remove('hide');
+        sec_historico.classList.add('sec-historico-on');
+        title.style.opacity = '1';
+
         play.classList.remove('button-show');
         play.classList.add('button-hide');
-        sec_historico.classList.remove('hide');
-        main_screen.style.width = '600px';
-        main_screen.style.height = '700px';
-        sec_historico.style.width = '550px';
-        sec_historico.style.height = '300px';
-        sec_historico.style.padding = '10px';
-        sec_historico.style.marginTop = '100px';
-        title.style.opacity = '1';
-        // sec_historico.style.marginTop = '100px';
+
         sair.style.bottom = '50px';
-        historico.style.pointerEvents = 'none';
     }
 });
 
 play.addEventListener('click', ()=>{
+    // main_screen.querySelector('h1').style.opacity = '0';
+
     game.classList.remove('hide');
+    
+    historico.classList.add('hide');
+    historico.classList.remove('button-show');
+
+    play.classList.remove('button-show');
+    play.classList.add('hide');
+
+    register.classList.remove('hide');
+    register.classList.remove('dimension-zero');
+    register.classList.add('dimension-full');
+
+    sair.style.bottom = '50px';
+});
+
+comecar.addEventListener('click', ()=>{
+    main_screen.classList.add('main-screen-alt-2');
+    main_screen.querySelector('h1').style.zIndex = '3';
+
+    register.classList.add('hide');
+    register.classList.add('dimension-zero');
+    register.classList.remove('dimension-full');
+
+    tela_jogo.classList.remove('dimension-zero');
+    tela_jogo.classList.add('dimension-full');
+
 });
 
 sair.addEventListener('click', ()=>{
 
     if(!sec_historico.classList.contains('hide')) {
-        sec_historico.style.width = '300px';
-        sec_historico.style.height = '0';
-        sec_historico.style.padding = '0';
+        main_screen.classList.remove('main-screen-alt');
+
+        historico.style.pointerEvents = 'initial';
+        sec_historico.classList.remove('sec-historico-on');
         sec_historico.classList.add('hide');
         title.style.opacity = '0';
-        main_screen.style.width = '400px';
-        main_screen.style.height = '500px';
+
         play.classList.remove('button-hide');
         play.classList.add('button-show');
-        sair.style.bottom = '105px';
-        historico.style.pointerEvents = 'initial';
+
+        sair.style.bottom = '105px';   
 
     }else if(!game.classList.contains('hide')){
+
+        if(!tela_jogo.classList.contains('hide')){
+            main_screen.classList.remove('main-screen-alt-2');
+            main_screen.querySelector('h1').style.zIndex = '1';
+
+            tela_jogo.classList.remove('dimension-full');
+            tela_jogo.classList.add('dimension-zero');
+        }
         game.classList.add('hide');
-        game.style.marginTop = '90px';
+
+        historico.classList.remove('hide');
+        historico.classList.add('button-show');
+
+        play.classList.remove('hide');
+        play.classList.add('button-show');
+
+        register.classList.remove('dimension-full');
+        register.classList.add('dimension-zero');
+
+        sair.style.bottom = '105px';
     }else {
         window.location.href = "about:blank";
     }
